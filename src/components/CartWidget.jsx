827ac60a39fb28babab64de05fd9ee56edbd0a22
Gina Pellegri <gina.pellegri@minsal.cl>
2021-12-06 18:50:29 -0300

    contador tarea 3

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function CartWidget(props) {
     return (
         <div>
-            <i class="fa fa-shopping-cart" aria-hidden="true"> 4</i>
+            <i className="fa fa-shopping-cart" aria-hidden="true"> 4</i>
         </div>
     );
   }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ItemCount from './ItemCount';
 
 function ItemListContainer({list}) {
     return (
         <div>
             <Container>
             <Row>
-                <Col sm>{list}</Col>
+                <Col sm>{list}
+                    <ItemCount stock="5" initial="1"/>
+                </Col>
                 <Col sm>Item 2</Col>
                 <Col sm>Item 3</Col>
             </Row>
